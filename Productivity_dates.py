20d3f7f8370146da1bf0cfc2cb7ebbcf2bcdc996
Juan-Flerovium <115925384+Juan-Flerovium@users.noreply.github.com>
2024-08-18 22:52:13 +0100

    Update Productivity_dates.py

--- Productivity_dates.py
+++ Productivity_dates.py
@@ -1738,7 +1738,7 @@ def plot_making3(a, b):
     flowlineactivityfig.update_traces(
         hovertemplate='Initial Date: %{customdata[0]}<br>Final Date: %{customdata[1]}<br>Activity: %{customdata[2]}',
     )
-    if [i for i in list(np.concatenate([i.x for i in flowlineactivityfig.data])) if i != None]!=[]:
+    if [i for i in list(np.concatenate([i.x for i in flowlineactivityfig.data], np.array([None]))) if i != None]!=[]:
           enddate = max(datereadjust5(i) for i in list(np.concatenate([i.x for i in flowlineactivityfig.data])) if type(i) == date or type(i) == datetime)
     else:
           #For the case that not enough data has been gathered
