ca3abf15a2a613c6f2d6cdba828807312a4bb0a5
Juan-Flerovium <115925384+Juan-Flerovium@users.noreply.github.com>
2024-08-19 00:42:28 +0100

    Update Productivity_dates.py

--- Productivity_dates.py
+++ Productivity_dates.py
@@ -370,6 +370,14 @@ def datereadjust6(x):
     else:
         return x.date()
 
+def nd(x):
+    if type(x) == np.ndarray:
+        return x
+    if type(x) == list:
+        return np.array(x)
+    else:
+        return np.array([x])
+
 def concatenatelists(x):
     y = []
     for i in x:
@@ -1745,7 +1753,7 @@ def plot_making3(a, b):
     flowlineactivityfig.update_traces(
         hovertemplate='Initial Date: %{customdata[0]}<br>Final Date: %{customdata[1]}<br>Activity: %{customdata[2]}',
     )
-    if [i for i in list(concatenatelists([i.x for i in flowlineactivityfig.data])) if i != None]!=[]:
+    if [i for i in list(concatenatelists([nd(i.x) for i in flowlineactivityfig.data])) if i != None]!=[]:
           enddate = max(datereadjust5(i) for i in list(np.concatenate([i.x for i in flowlineactivityfig.data])) if type(i) == date or type(i) == datetime)
     else:
           #For the case that not enough data has been gathered
