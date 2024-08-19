35a9e3dbf7bfce96b11d3795e41af091a2a2d760
Juan-Flerovium <115925384+Juan-Flerovium@users.noreply.github.com>
2024-08-19 12:00:21 +0100

    Update Renderexample.py

--- Renderexample.py
+++ Renderexample.py
@@ -5,13 +5,7 @@ from dash import html
 app = dash.Dash(__name__)
 server = app.server
 
-# Get the port from the environment variable
-port = int(os.environ.get("PORT", 8050))
-
 app.layout = html.Div("Hello, Render!")
-
-# Get the port from the environment variable
-port = int(os.environ.get("PORT", 8050))
 
 if __name__ == "__main__":
     # Get the port from the environment variable
