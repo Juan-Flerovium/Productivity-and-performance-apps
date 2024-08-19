d4a6958db212e1955d43ef1987fe7020ed232465
Juan-Flerovium <115925384+Juan-Flerovium@users.noreply.github.com>
2024-08-19 10:41:05 +0100

    Update Renderexample.py

--- Renderexample.py
+++ Renderexample.py
@@ -3,7 +3,6 @@ import dash
 import dash_html_components as html
 
 app = dash.Dash(__name__)
-server = app.server
 
 # Get the port from the environment variable
 port = int(os.environ.get("PORT", 8050))
@@ -15,3 +14,4 @@ port = int(os.environ.get("PORT", 8050))
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", port=port)
+    server = app.server
